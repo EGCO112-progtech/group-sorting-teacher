bebb861d9ae6212b772085443c696060ccd981a3
AjMing <62439174+AjMing@users.noreply.github.com>
2023-02-01 16:02:21 +0000

    first

--- main.c
+++ main.c
@@ -1,19 +1,31 @@
-#define N 6
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
 #include "sorting.h"
+int main(int argc,char **argv) {
+    int *a;//[N]={3,2,6,7,3,1};
+    int n=argc-2;
+    a=(int*)malloc(sizeof(int)*n);
+  int i,j=0;
+    for(i=0;i<n;i++){
+        a[i]=atoi(argv[i+2]);
+      
+          
+    }
+  
+ 
+  display(a,n);
+  if(strcmp(argv[1],"bubble")==0)
+      bubbleSort(a,n);
+else  if(strcmp(argv[1],"insertion")==0)
+      insertionSort(a,n);
 
-int main() {
-  int a[N]={3,2,6,7,3,1};
-  int i,j,new_number;
-
-   display(a,N);
-  bubbleSort(a,N); 
-   //insertion(a,N);
-
-  // selectionSort(a,N);
-   display(a,N);
+ else  if(strcmp(argv[1],"selection")==0) 
+         selectionSort(a,n);
+else {
+    printf("Incorrect type\n");
+}
+   display(a,n);
+    
  return 0;
 }
-
-
--- sorting.h
+++ sorting.h
@@ -1,7 +1,4 @@
-// selection sort function module in C
-
 void display(int a[],int n){
-
     int i;
 
     for(i=0;i<n;i++)
@@ -10,21 +7,7 @@ void display(int a[],int n){
   
 }
 
-void selectionSort(int data[], int length) 
-{ 
-	int i, j, m, mi; 
-	
-     display(data,length);
-     
-	
-} 
 
-
-
-void insertion(int a[],int n){
-
-
-}
 
 void swap(int *a,int *b){
  int temp;
@@ -34,18 +17,58 @@ void swap(int *a,int *b){
   
 }
 
+
+int insertionSort(int a[],int n){
+   int i,j,v;
+  
+    for(i=1;i<n;i++){
+        v=a[i];
+      for(j=i-1;j>=0 && a[j]<v;j--) {
+        a[j+1]=a[j];
+           display(a,n);
+      }
+      a[j+1]=v;
+         display(a,n);
+         printf("\n");
+      }
+    }
+  
+  
+
+void selectionSort(int a[],int n){
+int mi,i,j;
+    for(i=0;i<n-1;i++){
+        mi=i;
+      for(j=i+1;j<n;j++){
+        if(a[mi]<a[j]) mi=j;
+          
+        }
+      if(mi!=i) swap(&a[i],&a[mi]);
+      display(a,n);
+      printf("\n");
+    }
+
+}
+
 void bubbleSort(int a[],int n){
 
 int i,j;
 int sorted;
 // how may pair to compare?
 for(j=1;j<=n-1;j++){
- 
-  display(a,n);
+    sorted=1;
+    for(i=0;i<n-j;i++){
+        if(a[i]<a[i+1]) {
+            swap(&a[i],&a[i+1]);
+            sorted=0;
+        }
+      display(a,n);
+    }
+    if(sorted==1)break;
+    printf("\n");
+
  }
   
 }
-
-
 
 
